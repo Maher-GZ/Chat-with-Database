c1a0c2232a7095e56241448300de1548e5d30da2
Mahmoud <sab301146782298@gmail.com>
2024-11-18 15:15:45 +0200

    update

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,5 +1,15 @@
-from .app import app 
+import sys
 import os
-if __name__=="__name__":
-    port = int(os.environ.get("PORT",4000))
-    app.run(host="0.0.0.0",port=port)
+
+# Add the parent directory of the Backend folder to the system path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Import the Flask app from Backend.app
+from Backend.app import app
+
+if __name__ == "__main__":
+    # Define the port, defaulting to 4000 if no environment variable is set
+    port = int(os.environ.get("PORT", 4000))
+    
+    # Run the Flask app
+    app.run(host="0.0.0.0", port=port)
